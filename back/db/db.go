22ad8d1bfcffb77425785468c72d45adc52ef062
Raein Soltani <raeen.soltani@yahoo.com>
2024-07-07 01:11:06 +0330

    Initial commit chore: Add database initialization and migration code

---
+++ back/db/db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/raeinsoltani/gorello/back/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	var err error
+	dsn := fmt.Sprintf(
+		// "host=%s user=%s dbname=%s sslmode=disable password=%s",
+		// os.Getenv("DB_HOST"),
+		// os.Getenv("DB_USER"),
+		// os.Getenv("DB_NAME"),
+		// os.Getenv("DB_PASSWORD"),
+		"postgres://postgres:password@127.0.0.1:5432/test",
+	)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database!", err)
+	}
+	fmt.Println("Database connected")
+
+	err = DB.AutoMigrate(&models.User{}, &models.Contact{}, &models.Chat{}, &models.Message{})
+	if err != nil {
+		log.Fatal("Failed to migrate database!", err)
+	}
+	fmt.Println("Database Migrated")
+}
