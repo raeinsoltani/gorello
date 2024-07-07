32471837fed47673bec018f49e516e2ed4df367a
Raein Soltani <raeen.soltani@yahoo.com>
2024-07-07 09:01:44 +0330

    chore: Remove unused Chat model and its dependencies

---
+++ back/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo-contrib/echoprometheus"
+	"github.com/labstack/echo-contrib/jaegertracing"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/raeinsoltani/gorello/back/db"
+	"github.com/raeinsoltani/gorello/back/handlers"
+	customMiddleware "github.com/raeinsoltani/gorello/back/middleware"
+	"github.com/raeinsoltani/gorello/back/repository/gorm"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
+func main() {
+	time.Sleep(3 * time.Second)
+	db.Init()
+	e := echo.New()
+
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	c := jaegertracing.New(e, nil)
+	defer func(c io.Closer) {
+		err := c.Close()
+		if err != nil {
+			log.Printf("Failed to close tracer: %v\n", err)
+		}
+	}(c)
+
+	e.Use(middleware.Logger())
+	// e.Use(middleware.CSRF())
+	e.Use(middleware.CORS())
+	e.Use(echoprometheus.NewMiddleware("gorello"))
+
+	e.GET("/metrics", echoprometheus.NewHandler())
+
+	e.GET("/", func(c echo.Context) error {
+		log.Println("Handling the request for the root route")
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	userRepo := gorm.NewUserRepo(db.DB)
+	workspaceRepo := gorm.NewWorkspaceRepo(db.DB)
+	userWorkspaceRoleRepo := gorm.NewUserWorkspaceRoleRepo(db.DB)
+
+	userHandler := handlers.NewUserHandler(userRepo)
+	workspaceHandler := handlers.NewWorkspaceHandler(workspaceRepo, userWorkspaceRoleRepo, userRepo)
+
+	e.GET("/healthz", func(c echo.Context) error { return c.NoContent(http.StatusNoContent) })
+
+	auth := e.Group("/auth")
+	users := e.Group("/users")
+	workspaces := e.Group("/workspaces")
+
+	// User auth Handlers
+	auth.POST("/signup", userHandler.Register)
+	auth.POST("/login", userHandler.Login)
+
+	// Users Handlers
+	users.Use(customMiddleware.JWTAuthentication)
+	users.GET("/", userHandler.GetUsers)
+	users.GET("/:username", userHandler.GetUser)
+	users.PUT("/:username", userHandler.UpdateUser)
+	users.DELETE("/:username", userHandler.DeleteUser)
+	users.GET("/search", userHandler.SearchUsers)
+
+	// Workspaces Handlers
+	workspaces.Use(customMiddleware.JWTAuthentication)
+	workspaces.POST("/", workspaceHandler.CreateWorkspace)
+	workspaces.GET("/:workspaceId", workspaceHandler.GetWorkspaceDescription)
+
+	log.Println("Starting Echo server on port 8080...")
+	e.Logger.Fatal(e.Start(":8080"))
+}
